3dbd6802f7050188df366b39316c64f4a7feab5d
Ravi Kumar Gupta <111415169+EnggAdda@users.noreply.github.com>
2023-04-06 15:39:28 +0530

    git demo app

---
+++ src/test/java/com/example/githubdemoconflict/GitHubDemoConflictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubdemoconflict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubDemoConflictApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
